3a899c9b62619b28866b1cd95cbaab092b113bf7
walshj9 <walshj9@students.rowan.edu>
2020-05-21 15:53:15 -0400

    First commit, notes, and source

---
+++ process.cpp
@@ -0,0 +1,18 @@
+//data source:https://datahub.io/core/s-and-p-500-companies-financials#resource-constituents-financials
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+	
+	
+	return 0;
+}
+/*notes for larger values
+ * Market cap values are long long int
+ * EBITDA values are long long int
+ */
+
+
+
